05b17c29c9e90fa345397acabebe93d32c063945
Gaétan Manigold <gaetanmanigold6@gmail.com>
2022-06-16 10:16:00 +0200

    Creéation de ma route en V6

---
+++ src/Root.js
@@ -0,0 +1,26 @@
+import React from "react";
+import App from "./App"
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+const Root = () => {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" />
+          {/* /: pour dire la suite de l'url dans le paramètre */}
+          {/* <Route path="" component={}/> */}
+          <Route />
+          {/* <Route path="/company/:name" component={MovieCompanies} /> */}
+          {/* Si aucune url trouvé */}
+          {/* <Route component={NotFound}/> */}
+  
+        </Routes>
+      </BrowserRouter>
+    )
+  }
+  
+  export default Root
