7e2cdad63f4bfafc6821a519987d8748a32d8925
Gaétan Manigold <gaetanmanigold6@gmail.com>
2022-06-16 09:47:21 +0200

    Ajout du tableau de mes Pizzas

---
+++ src/pizzas.js
@@ -0,0 +1,34 @@
+import piz1 from "./img/4-fromages.png"
+import piz2 from "./img/marguerita.png"
+import piz3 from "./img/pepperoni.png"
+import piz4 from "./img/reine.png"
+
+const pizzas = [
+    {
+        id: 1,
+        name: "4 Fromages",
+        price:9.80,
+        picture: piz1
+    },
+    {
+        id: 2,
+        name: "Marguerita",
+        price:7.90,
+        picture: piz2
+    },
+    {
+        id: 3,
+        name: "Pepperoni",
+        price:8.80,
+        picture: piz3
+    },
+    {
+        id: 4,
+        name: "Reine",
+        price:10.50,
+        picture: piz4
+        
+    },
+];
+
+export default pizzas;
