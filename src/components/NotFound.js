2e10bcf132043c1137bb050bf97b52a19155bf6b
Florian Cagnon <florian.cagnon@live.fr>
2022-06-16 10:24:04 +0200

    ajout de la page NotFound

---
+++ src/components/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>Lache-moi les bask si tu veux pas prendre mon pied au cul !</h1>
+    </div>
+  )
+}
+
+export default NotFound
