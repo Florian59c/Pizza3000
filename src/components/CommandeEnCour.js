da4eaab2e3c06b1784b5dbfe510adff31e58aee3
Florian Cagnon <florian.cagnon@live.fr>
2022-06-17 10:38:31 +0200

    ajout du app.js avec css + les autres pages avec le header intégré

---
+++ src/components/CommandeEnCour.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './Header';
+
+const CommandeEnCour = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
+
+export default CommandeEnCour
